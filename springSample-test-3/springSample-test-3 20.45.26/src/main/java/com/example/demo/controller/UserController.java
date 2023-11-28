342bc7b05d73fa58aa30034e2de09563fad1d8ab
haru640 <136230611+haru640@users.noreply.github.com>
2023-11-28 20:58:14 +0900

    Add files via upload

---
+++ springSample-test-3/springSample-test-3 20.45.26/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,63 @@
+package com.example.demo.controller;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.example.demo.entity.UserEntity;
+import com.example.demo.service.UserService;
+
+/**
+ * ユーザー情報 Controller
+ */
+@Controller
+public class UserController {
+  /**
+   * ユーザー情報 Service
+   */
+  //2行追加
+	@Autowired
+	private UserService userService;
+  /**
+   * ユーザー情報一覧画面を表示
+   * ＠param  model Model
+   * @return  
+   * ユーザー情報一覧画面のHTML
+   */
+	@GetMapping("/user/list")
+	public String displayList(Model model) {
+
+    //3行追加
+	  List<UserEntity> userList = userService.searchAll(); // Change this according to your service method
+	  model.addAttribute("userlist", userList);
+	  return "user/list"; // Assuming that "user/list" is the correct HTML template pat
+
+
+
+  }
+	
+	/**
+	 * ユーザー新規登録画面を表示
+	 * @param  model Model
+	 * @return  ユーザー情報一覧画面
+	 */
+	@GetMapping("/user/add")
+	public String displayAdd(Model model) {
+		return "user/add";
+	}
+	
+	/**
+	 * ユーザー情報詳細画面を表示
+	 * @param  id 表示するユーザーID
+	 * @param  model Model
+	 * @return  ユーザー情報詳細画面
+	 */
+	@GetMapping("/user/{id}")
+	public String displayView(@PathVariable  String id, Model model) {
+		return "user/view";
+
+	}	
+}
