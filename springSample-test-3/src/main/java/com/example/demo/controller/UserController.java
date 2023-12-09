b1fd131748d87673d6323cb101c5f52349753b5b
haru640 <136230611+haru640@users.noreply.github.com>
2023-12-09 15:35:02 +0900

    Add files via upload

--- springSample-test-3/src/main/java/com/example/demo/controller/UserController.java
+++ springSample-test-3/src/main/java/com/example/demo/controller/UserController.java
@@ -1,4 +1,5 @@
 package com.example.demo.controller;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,28 +23,28 @@ import com.example.demo.service.UserService;
  */
 @Controller
 public class UserController {
-  private static final DefaultMessageSourceResolvable error = null;
-/**
-   * ユーザー情報 Service
-   */
-  //2行追加
+	private static final DefaultMessageSourceResolvable error = null;
+	/**
+	 * ユーザー情報 Service
+	 */
+	//2行追加
 	@Autowired
 	private UserService userService;
-  /**
-   * ユーザー情報一覧画面を表示
-   * ＠param  model Model
-   * @return  
-   * ユーザー情報一覧画面のHTML
-   */
+	/**
+	 * ユーザー情報一覧画面を表示
+	 * ＠param  model Model
+	 * @return  
+	 * ユーザー情報一覧画面のHTML
+	 */
 	@GetMapping("/user/list")
 	public String displayList(Model model) {
 
-    //3行追加
-	  List<UserEntity> userlist = userService.searchAll(); // Change this according to your service method
-	  model.addAttribute("userlist", userlist);
-	  return "user/list"; // Assuming that "user/list" is the correct HTML template pat
-      }
-	
+		//3行追加
+		List<UserEntity> userlist = userService.searchAll(); // Change this according to your service method
+		model.addAttribute("userlist", userlist);
+		return "user/list"; // Assuming that "user/list" is the correct HTML template pat
+	}
+
 	/**
 	 * ユーザー新規登録画面を表示
 	 * @param  model Model
@@ -51,11 +52,11 @@ public class UserController {
 	 */
 	@GetMapping("/user/add")
 	public String displayAdd(Model model) {
-	model.addAttribute("userRequest", new UserRequest());
+		model.addAttribute("userRequest", new UserRequest());
 		//一行追加
-	return "user/add";
+		return "user/add";
 	}
-	
+
 	/**
 	 * ユーザー新規登録
 	 * @param  userRequest リクエストデータ
@@ -64,28 +65,28 @@ public class UserController {
 	 */
 	@GetMapping("/user/create")
 	public String create(@Validated @ModelAttribute UserRequest userRequest, BindingResult result, 
-	Model model) {
-	
-		
+			Model model) {
+
+
 		//入力判定入れること
 		if(result.hasErrors()) {
-			
-		List<String>errorList=new ArrayList<String>();
-		for (@SuppressWarnings("unused") ObjectError error:result.getAllErrors());{
-		errorList.add(error.getDefaultMessage());
-	      }
+
+			List<String> errorList = new ArrayList<String>();
+			for (ObjectError error :result.getAllErrors());{
+				errorList.add(error.getDefaultMessage());
+			}
 			// 入力チェックエラーの場合3行実装
-    
-//エラー判定後の画面遷移2行実装
-		model.addAttribute("validationError",error);
-		return"redirect:/user/list";
+
+			//エラー判定後の画面遷移2行実装
+			model.addAttribute("validationError",errorList);
+			return"user/add";
 		}
 		// ユーザー情報の登録2行実装
-		 userService.create(userRequest);
-         return"user/add";
-         
-         }
-	
+		userService.create(userRequest);
+		return"redirect:/user/list";
+
+	}
+
 	/**
 	 * ユーザー情報詳細画面を表示
 	 * @param  id 表示するユーザーID
@@ -97,5 +98,5 @@ public class UserController {
 		return "user/view";
 
 	}
-	}
-	
+}
+
